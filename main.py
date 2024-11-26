1496160bc9ae981c2eb31a8c92ac6251cfba1ed1
mrpathan8840 <harshuraja001@gmail.com>
2024-11-26 20:06:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def send_message():
     label { color: white; }
     .file { height: 30px; }
     body {
-      background-image: url('https://ibb.co/JxvM3Qf');
+      background-image: url('https://i.ibb.co/LRrPTkG/c278d531d734cc6fcf79165d664fdee3.jpg');
       background-size: cover;
       background-repeat: no-repeat;
       color: white;
